6aa26b2d8a7c51fb61ea7f40f331e98d24d73b04
✿ -mameaw- ✿ <chutinunpoolsup@MacBook-Pro-khxng-Chutinun.local>
2023-09-02 22:40:37 +0700

    Intiial commit

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.symmetric(
-            horizontal: 20.0,
+            horizontal: 40.0,
             vertical: 20.0,
           ),
           decoration: BoxDecoration(
